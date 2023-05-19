5ab3b256dda40cc41452017cc3682d1d1893a156
SofidevO <angiemeow89@gmail.com>
2023-05-19 04:38:29 -0600

    Changes to be committed: 	new file:   img/anime01.png 	new file:   img/anime02.png 	modified:   index.html 	modified:   js/encriptador.js 	modified:   styles/style.css

--- js/encriptador.js
+++ js/encriptador.js
@@ -3,6 +3,7 @@ const d = document;
 const mensajeResultado = document.getElementById('resultado');
 const areaTexto = (document.getElementById("encriptador"));
 const ImagenMuneco = d.getElementById('muneco');
+const ImagenForbriden = d.getElementById('forbriden');
 const Leyenda = d.getElementById('leyenda');
 
 
@@ -14,42 +15,73 @@ const btnDesencriptar = d.getElementById('desencriptar');
 
 const btnEncriptar = d.getElementById('encriptar') ;
 
-
+ImagenForbriden.style.display = 'none'; 
 /* funcion btnEncriptar, boton */
 export default  btnEncriptar.addEventListener('click', ()=>{
+    
     let area = recuperarTexto();
     let encriptartexto = encriptar();
-
     
+
+
     function encriptar(){
         if (recuperarTexto() === ''){
-            alert("Debes ingresar texto")
-            
+            mensajeResultado.textContent = ErrorAlert();
+            ImagenForbriden.style.display = 'block'; 
+            Leyenda.style.display = 'none'; 
+            ImagenMuneco.style.display = 'none';
+            const loader = d.querySelector(".loader");
+            loader.classList.add("none");
+ 
         }else{
-            alert(recuperarTexto())
+            ImagenForbriden.style.display = 'none'; 
+            ImagenMuneco.style.display = 'none'; 
+
+            mensajeResultado.textContent = recuperarTexto();
             
+            const loader = d.querySelector(".loader");
+            loader.classList.add("none");
+
+
         }
     }
-    
+
     function recuperarTexto(){
-        let area = d.querySelector(".area"); 
+        let area = d.querySelector(".area");
+        ImagenMuneco.style.display = 'none' 
+ 
         return area.value;
     }
-
-    
-        
 })
+
+/* Funciones que cambian el texto: resultado */
+function ErrorAlert(){
+    let textoError = d.querySelector(".resultado"); 
+    return ("Debes ingresar un texto");
+    const loader = d.querySelector(".loader");
+    loader.classList.add("none");
+}
+function capturandoTexto(){
+    let capturando = d.querySelector(".resultado");
+    return ("Capturando Texto");
+
+}
+
 
 
 /* al poner el cursor en el text area, desaparace el personaje (muneco) y la leyenda y aparece un loader */
 
 areaTexto.addEventListener('click', ()=>{
-    Leyenda, ImagenMuneco.style.display = 'none' 
+    ImagenMuneco.style.display = 'none' 
+    ImagenForbriden.style.display = 'none' 
     Leyenda.style.display = 'none'; 
     const loader = d.querySelector(".loader");
     loader.classList.remove("none");
+    mensajeResultado.textContent = capturandoTexto();
+
 
 })
+
 
 /* Boton para limpiar TransformStreamDefaultController(Solo es un reset) */
 
@@ -60,6 +92,11 @@ btnLimpiar.addEventListener('click', ()=>{
     }
 
 })
+
+
+
+
+
 
 
 
