fd6464ec55fa7201eb09e9a6791a67520d55a12e
SofiDevO <angiemeow89@gmail.com>
2023-05-20 03:25:28 -0600

    Agregando funciones al evento areaTexto, para evitar que el usuario ingrese carácters especiales, Mayúsculas o más de 23 letras seguidas

--- js/encriptador.js
+++ js/encriptador.js
@@ -15,12 +15,14 @@ const btnDesencriptar = d.getElementById('desencriptar');
 
 const btnEncriptar = d.getElementById('encriptar') ;
 
+
 ImagenForbriden.style.display = 'none'; 
 /* funcion btnEncriptar, boton */
 export default  btnEncriptar.addEventListener('click', ()=>{
-    
     let area = recuperarTexto();
     let encriptartexto = encriptar();
+    
+    
     
 
 
@@ -34,56 +36,93 @@ export default  btnEncriptar.addEventListener('click', ()=>{
             loader.classList.add("none");
  
         }else{
+            
             ImagenForbriden.style.display = 'none'; 
             ImagenMuneco.style.display = 'none'; 
 
-            mensajeResultado.textContent = recuperarTexto();
             
+            mensajeResultado.textContent = recuperarTexto();
             const loader = d.querySelector(".loader");
             loader.classList.add("none");
-
-
         }
     }
+   
 
-    function recuperarTexto(){
-        let area = d.querySelector(".area");
-        ImagenMuneco.style.display = 'none' 
- 
-        return area.value;
-    }
-})
+            
 
-/* Funciones que cambian el texto: resultado */
+
+    
+    
+    
+    
+    
+});
+
+/* Función para recuperar el texto del área de texto */
+function recuperarTexto(){
+    let area = d.querySelector(".area");
+    ImagenMuneco.style.display = 'none' 
+   
+    return area.value.toLowerCase();
+}
+
+
+
+
+/* Función para mostrar un mensaje de error */
 function ErrorAlert(){
     let textoError = d.querySelector(".resultado"); 
     return ("Debes ingresar un texto");
     const loader = d.querySelector(".loader");
     loader.classList.add("none");
 }
+
+/* Función para mostrar un mensaje de capturando texto */
+
 function capturandoTexto(){
     let capturando = d.querySelector(".resultado");
     return ("Capturando Texto");
+    
 
 }
 
 
 
-/* al poner el cursor en el text area, desaparace el personaje (muneco) y la leyenda y aparece un loader */
+/* Evento al hacer clic en el área de texto */
 
 areaTexto.addEventListener('click', ()=>{
+    
     ImagenMuneco.style.display = 'none' 
     ImagenForbriden.style.display = 'none' 
     Leyenda.style.display = 'none'; 
     const loader = d.querySelector(".loader");
     loader.classList.remove("none");
     mensajeResultado.textContent = capturandoTexto();
-
 })
 
 
+/* Evento al escribir en el área de texto */
+areaTexto.addEventListener('input', () => {
+    let texto = recuperarTexto();
+    
+    texto = texto.replace(/[^\w\s]/gi, '');
+    areaTexto.value = texto;
+    if (texto.length > 16) {
+      const palabras = texto.split(' ');
+      const palabrasCortas = palabras.filter(palabra => palabra.length <= 17);
+      areaTexto.value = palabrasCortas.join(' ');
+    }
+});    
+    
 
-/* Boton para limpiar TransformStreamDefaultController(Solo es un reset) */
+
+
+
+
+
+
+
+/* Evento al hacer clic en el botón de limpiar */
 
 btnLimpiar.addEventListener('click', ()=>{
     reinicio = limpiar();
@@ -92,7 +131,6 @@ btnLimpiar.addEventListener('click', ()=>{
     }
 
 })
-
 
 
 
