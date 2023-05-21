5508740abde92f2ae6e0cbcecd92d738213fd0f8
SofiDevO <angiemeow89@gmail.com>
2023-05-21 15:53:39 -0600

    Ajustes en los estilos al cambiar entre dark theme y normal

--- js/neonTheme.js
+++ js/neonTheme.js
@@ -4,6 +4,8 @@ const d = document;
 const toggle = d.getElementById ('toggleDark');
 const body = d.querySelector('body');
 const cajaResultado = d.getElementById('mensaje-resultado');
+const logo = d.getElementById('logo');
+const logoFooter = d.getElementById('logo-footer');
 const mensajeResultado = d.getElementById('resultado')
 const botonEncriptar= d.getElementById('encriptar')
 const botonPegar= d.getElementById('pegar')
@@ -20,6 +22,13 @@ export  function cambiarModo(){
     if(this.classList.toggle('bi-lightbulb')){
         body.style.background = 'white';
         body.style.color = 'black';
+        
+        logo.style.filter = 'none';
+        logoFooter.style.filter = 'none';
+        
+
+        cajaResultado.style.boxShadow = 'none';
+
         body.style.transition = '2s';
     }else{
         body.style.background = 'black';
@@ -30,27 +39,49 @@ export  function cambiarModo(){
         areaTexto.style.backgroundColor = 'black'
         areaTexto.style.border = 'solid 3px rgba(0,212,255,1)';
         areaTexto.style.boxShadow = '0 0 20px rgba(0,212,255,1)';
+        areaTexto.style.transition = '2s';
+        
+        
+        logo.style.filter = 'invert(100%) sepia(12%) saturate(166%) hue-rotate(219deg) brightness(115%) contrast(90%) drop-shadow(0 0 12px  rgba(0,212,255,1)';
+        
+        mensajeResultado.style.color = 'white'
+        mensajeResultado.style.transition = '2s';
+
+        
+        logoFooter.style.filter = 'invert(100%) sepia(12%) saturate(166%) hue-rotate(219deg) brightness(115%) contrast(90%) drop-shadow(0 0 12px  rgba(0,212,255,1)';
 
         mensajeResultado.style.color = 'white'
+        mensajeResultado.style.transition = '2s';
         
         cajaResultado.style.border = 'solid 4px rgba(0,212,255,1)';
         cajaResultado.style.boxShadow = '0 0 20px rgba(0,212,255,1)';
+        cajaResultado.style.transition = '2s';
 
 
         botonEncriptar.style.color = 'rgba(0,212,255,1)'
         botonEncriptar.style.boxShadow = '0 0 10px  white'
+        botonEncriptar.style.transition = '2s';
+
 
         botonPegar.style.color = 'rgba(0,212,255,1)'
         botonPegar.style.boxShadow = '0 0 10px  white'
+        botonPegar.style.transition = '2s';
+
+
 
         botonLimpiar.style.color = 'rgba(0,212,255,1)'
         botonLimpiar.style.boxShadow = '0 0 10px  white'
+        botonLimpiar.style.transition = '2s';
 
-        botonCopiar.style.color = 'rgba(0,212,255,1)'
-        botonCopiar.style.boxShadow = '0 0 10px  white'
 
-        botonDesencriptar.style.color = 'rgba(0,212,255,1)'
-        botonDesencriptar.style.boxShadow = '0 0 10px  white'
+        botonCopiar.style.color = 'rgba(0,212,255,1)';
+        botonCopiar.style.boxShadow = '0 0 10px  white';
+        botonCopiar.style.transition = '2s';
+
+        botonDesencriptar.style.color = 'rgba(0,212,255,1)';
+        botonDesencriptar.style.boxShadow = '0 0 10px  white';
+        botonDesencriptar.style.transition = '2s';
+
 
         botonEncriptar.style.background = 'linear-gradient(50deg, rgba(2,0,36,1) 0%, rgba(10,77,104,1) 50%, rgba(0,212,255,1) 100%)'
         botonPegar.style.background = 'linear-gradient(50deg, rgba(2,0,36,1) 0%, rgba(10,77,104,1) 50%, rgba(0,212,255,1) 100%)'
