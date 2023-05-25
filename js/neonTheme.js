34c12990207a3fbe982c3416b84a4003973823e8
SofiDevO <angiemeow89@gmail.com>
2023-05-24 20:02:29 -0600

    Se modifica el tema oscuro

--- js/neonTheme.js
+++ js/neonTheme.js
@@ -2,6 +2,7 @@
 const d = document;
 /* Elementos de la pagina */
 const toggle = d.getElementById ('toggleDark');
+const header = d.getElementById ('header');
 const body = d.querySelector('body');
 const cajaResultado = d.getElementById('mensaje-resultado');
 const areaTexto = d.getElementById("encriptador")
@@ -15,8 +16,11 @@ const botonEncriptar= d.getElementById('encriptar')
 const botonPegar= d.getElementById('pegar')
 const botonLimpiar= d.getElementById( 'limpiar' )
 const botonDesencriptar= d.getElementById('desencriptar')
-/* const btnReset= d.getElementById('reset')
- *//* const botonCopiar= d.getElementById('copiar') */
+const botonGithub = d.getElementById('github')
+const botonAlura = d.getElementById('aluracursos')
+const botonLinkedin = d.getElementById('linkedin')
+const botonPlatzi = d.getElementById('platzi')
+
 
 
 toggle.addEventListener('click', cambiarModo);
@@ -27,13 +31,26 @@ export  function cambiarModo(){
         body.style.background = 'white';
         body.style.color = 'black';
         body.style.transition = '2s';
+      
+        header.style.backgroundColor = 'white';
+        header.style.transition = '2s';
+      
         toggle.style.filter = 'invert(0%) sepia(83%) saturate(7433%) hue-rotate(290deg) brightness(80%) contrast(114%);'
-        logo.style.filter = 'invert(16%) sepia(45%) saturate(2765%) hue-rotate(196deg) brightness(94%) contrast(97%)';
-        logoFooter.style.filter = 'invert(100%) sepia(0%) saturate(0%) hue-rotate(6deg) brightness(101%) contrast(104%)';
+       
+        logo.style.filter = ' invert(0%) sepia(99%) saturate(3%) hue-rotate(150deg) brightness(102%) contrast(100%)';
+        
+        logoFooter.style.filter = 'invert(100%) sepia(0%) saturate(0%) hue-rotate(6deg) brightness(101%) contrast(104%) drop-shadow(0 0 12px  rgba(0,212,255,1)';
+       
+        botonGithub.style.filter = 'invert(100%) sepia(0%) saturate(0%) hue-rotate(6deg) brightness(101%) contrast(104%)';
+        botonAlura.style.filter = 'invert(15%) sepia(73%) saturate(1667%) hue-rotate(197deg) brightness(99%) contrast(101%)';
+        botonLinkedin.style.filter = ' invert(15%) sepia(60%) saturate(6697%) hue-rotate(221deg) brightness(100%) contrast(97%)';
+        botonPlatzi.style.filter = 'invert(55%) sepia(47%) saturate(5746%) hue-rotate(64deg) brightness(99%) contrast(80%)';
+        
         areaTexto.style.backgroundColor = 'white'
         areaTexto.style.color = 'black';
         areaTexto.style.border = 'solid 4px #012650';
         areaTexto.style.boxShadow = '0 0 0px ';
+        
         mensajeResultado.style.color = 'black'
 
 
@@ -45,9 +62,17 @@ export  function cambiarModo(){
         /* Estilos en el tema oscuro */
     }else{
         body.style.background = 'black';
+        header.style.background = 'black';
+        header.style.transition = '2s';
         body.style.color = 'white';
         body.style.transition = '2s';
-
+       
+        botonGithub.style.filter = 'invert(100%) sepia(100%) saturate(0%) hue-rotate(289deg) brightness(103%) contrast(101%)drop-shadow(0 0 12px  rgba(255, 255, 255)';
+        botonAlura.style.filter = 'invert(16%) sepia(45%) saturate(2765%) hue-rotate(196deg) brightness(94%) contrast(97%) drop-shadow(0 0 12px  rgba(0,212,255,1)';
+        botonLinkedin.style.filter = 'invert(15%) sepia(60%) saturate(6697%) hue-rotate(221deg) brightness(100%) contrast(97%) drop-shadow(0 0 12px  rgba(0, 24, 179)';
+        botonPlatzi.style.filter = 'invert(55%) sepia(47%) saturate(5746%) hue-rotate(64deg) brightness(99%) contrast(80%) drop-shadow(0 0 12px  rgba(87, 196, 1)';
+       
+       
         areaTexto.style.color = 'white'
         areaTexto.style.backgroundColor = 'black'
         areaTexto.style.border = 'solid 3px rgba(0,212,255,1)';
@@ -55,7 +80,8 @@ export  function cambiarModo(){
         areaTexto.style.transition = '2s';
         
         
-        logo.style.filter = 'invert(100%) sepia(12%) saturate(166%) hue-rotate(219deg) brightness(115%) contrast(90%) drop-shadow(0 0 12px  rgba(0,212,255,1)';
+        logo.style.filter = 'invert(100%) sepia(0%) saturate(0%) hue-rotate(6deg) brightness(101%) contrast(104%) drop-shadow(0 0 12px  rgba(0,212,255,1)';
+        
 
         mensajeResultado.style.color = 'white'
         mensajeResultado.style.transition = '2s';
@@ -71,33 +97,21 @@ export  function cambiarModo(){
         cajaResultado.style.transition = '2s';
 
 
-        botonEncriptar.style.color = 'rgba(0,212,255,1)'
-        botonEncriptar.style.boxShadow = '0 0 10px  white'
-        botonEncriptar.style.transition = '.5s';
+        botonEncriptar.style.boxShadow = '0 0 10px  rgba(0,212,255,1)'
+        botonEncriptar.style.transition = '.4s';
 
 
-        botonPegar.style.color = 'rgba(0,212,255,1)'
-        botonPegar.style.boxShadow = '0 0 10px  white'
-        botonPegar.style.transition = '.5s';
+        botonPegar.style.boxShadow = '0 0 10px  rgba(0,212,255,1)'
+        botonPegar.style.transition = '.4s';
 
 
 
-        botonLimpiar.style.color = 'rgba(0,212,255,1)'
-        botonLimpiar.style.boxShadow = '0 0 10px  white'
-        botonLimpiar.style.transition = '.5s';
+        botonLimpiar.style.boxShadow = '0 0 10px  rgba(0,212,255,1)'
+        botonLimpiar.style.transition = '.4s';
 
-
-        /* botonCopiar.style.color = 'rgba(0,212,255,1)';
-        botonCopiar.style.boxShadow = '0 0 10px  white';
-        botonCopiar.style.transition = '2s'; */
+        botonDesencriptar.style.boxShadow = '0 0 10px  rgba(0,212,255,1)';
+        botonDesencriptar.style.transition = '.4s';
         
-       /*  btnReset.style.color = 'rgba(0,212,255,1)';
-        btnReset.style.boxShadow = '0 0 10px  white';
-        btnReset.style.transition = '2s'; */
-
-        botonDesencriptar.style.color = 'rgba(0,212,255,1)';
-        botonDesencriptar.style.boxShadow = '0 0 10px  white';
-        botonDesencriptar.style.transition = '2s';
 
 
         botonEncriptar.style.background = 'linear-gradient(50deg, rgba(2,0,36,1) 0%, rgba(10,77,104,1) 50%, rgba(0,212,255,1) 100%)'
